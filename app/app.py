8b31b83166d36996ad2ba10929b2c90985f55087
Salinas04 <antonio.salinas.ceb@immune.institute>
2024-10-31 21:19:26 +0100

    Last commit

--- app/app.py
+++ app/app.py
@@ -2,11 +2,9 @@ import gradio as gr
 import xgboost as xgb
 import pandas as pd
 
-# Cargar el modelo preentrenado
 model = xgb.Booster()
 model.load_model('models/Modelo_2.bst')
 
-# Definir únicamente las columnas que el modelo espera recibir
 model_columns = [
     'rfc_cliente', 'rfc_deudor', 'id_division_deudor', 'tipo_cliente', 'giro_cliente', 
     'id_division_documento', 'num_zona', 'pedido', 'id_c_deudor', 'id_c_cliente', 
@@ -17,25 +15,18 @@ model_columns = [
 ]
 
 def realizar_prediccion(file):
-    # Leer archivo de entrada
     input_df = pd.read_csv(file.name)
 
-    # Convertir columnas categóricas a tipo 'category'
     for col in input_df.select_dtypes(include=['object']).columns:
         input_df[col] = input_df[col].astype('category')
 
-    # Asegurarse de que solo se incluyan las columnas esperadas en el modelo
     input_df = input_df[model_columns]
-
-    # Rellenar cualquier valor NaN con 0, en caso de que existan valores faltantes
     input_df = input_df.fillna(0)
 
-    # Crear el DMatrix para la predicción
     dmatrix = xgb.DMatrix(input_df, enable_categorical=True)
     prediccion = model.predict(dmatrix)
     return prediccion.tolist()
 
-# Configuración de la interfaz de Gradio
 interfaz = gr.Interface(
     fn=realizar_prediccion,
     inputs=gr.inputs.File(label="Sube tu archivo CSV"),
